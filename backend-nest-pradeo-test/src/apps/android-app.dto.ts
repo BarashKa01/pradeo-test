89dd417082a8e399601373f030d22fd584cb3f16
Cyrille Nicaise <cyrille.ni1@gmail.com>
2022-05-13 16:46:28 +0200

    PUT and DELETE app route added

--- backend-nest-pradeo-test/src/apps/android-app.dto.ts
+++ backend-nest-pradeo-test/src/apps/android-app.dto.ts
@@ -1,3 +1,12 @@
+import { User } from "src/users/user.entity";
+
 export class AndroidAppDto {
+    id: number;
     name: string;
+    hash: string;
+    comment: string;
+    is_safe: boolean;
+    is_verified: boolean;
+    user: User;
+
 }
--- backend-nest-pradeo-test/src/apps/android-apps.controller.ts
+++ backend-nest-pradeo-test/src/apps/android-apps.controller.ts
@@ -1,23 +1,24 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller, Param, Post, Get, UseInterceptors, UploadedFile, Sse, MessageEvent } from '@nestjs/common';
+import { Controller, Param, Post, Get, UseInterceptors, UploadedFile, Sse, MessageEvent, Delete, Put, Body, Response } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { fileTypeFilter } from 'src/utils/file-upload.utils';
+import { fileTypeFilter } from 'src/utils/file.utils';
 import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { AndroidApp } from './android-app.entity';
 import { AndroidAppsService } from './android-apps.service';
-import { renameAppwithHash } from 'src/utils/file-upload.utils';
+import { renameAppwithHash } from 'src/utils/file.utils';
 import { CheckAppEvent } from 'src/events/check-app-event.event';
-import { interval, map, Observable } from 'rxjs';
+import { from, interval, map, Observable, Observer, of, switchMap } from 'rxjs';
 import { OnEvent } from '@nestjs/event-emitter';
+import { AndroidAppDto } from './android-app.dto';
 
 
 @Controller('android-apps')
 export class AndroidAppsController {
 
   constructor(private readonly usersService: UsersService, private readonly androidAppsService: AndroidAppsService
-    , private httpService: HttpService, private readonly checkAppEvent: CheckAppEvent) {}
+    , private httpService: HttpService, private readonly checkAppEvent: CheckAppEvent) { }
 
   //Retrieve all apps from the user id, since no authentication system is used in this exercice
   @Get('/from_user/:id')
@@ -32,10 +33,10 @@ export class AndroidAppsController {
     }),
     fileFilter: fileTypeFilter,
   }))
-  async createApp(@Param('id') id, @UploadedFile() file: Express.Multer.File): Promise<AndroidApp | string> {
+  async createApp(@Param('id') userId, @UploadedFile() file: Express.Multer.File): Promise<AndroidApp | string> {
 
     //First, check if the user exist
-    const relatedUser: User | null = await this.usersService.findOne(id);
+    const relatedUser: User | null = await this.usersService.findOne(userId);
 
     if (relatedUser !== undefined && relatedUser !== null) {
       const newFilePath = await renameAppwithHash(file.filename);
@@ -46,15 +47,13 @@ export class AndroidAppsController {
       newAndroidApp.hash = file.filename;
       newAndroidApp.is_verified = false;
       newAndroidApp.is_safe = false;
-      newAndroidApp.comment = '';
+      newAndroidApp.comment = 'Renseignez un commentaire';
       newAndroidApp.user = relatedUser;
 
       newAndroidApp = await this.androidAppsService.create(newAndroidApp);
 
       if (newAndroidApp.id !== undefined && newAndroidApp.id !== null) {
-        this.checkAppEvent.emitDoScan({app: newAndroidApp, filePath: newFilePath});
-
-        newAndroidApp.comment = "The application is successfuly uploaded, you can check the status in the store";
+        this.checkAppEvent.emitDoScan({ app: newAndroidApp, filePath: newFilePath });
         return newAndroidApp;
       }
       else {
@@ -66,19 +65,49 @@ export class AndroidAppsController {
     }
   }
 
+  @Put('/put')
+  async putApp(@Body() putAndroidApp: AndroidAppDto): Promise<AndroidApp> {
+    Response
+    return await this.androidAppsService.update(putAndroidApp);
+  }
+
+  @Delete('delete/:id')
+  async deleteApp(@Param('id') appId: string): Promise<any> {
+    const id = String(appId);
+
+    //Take the result of affected entity by deletion
+    const result = await this.androidAppsService.remove(appId).then(response => { return response.affected });
+    if (result === 0) {
+      return { message: "0" };
+    } else {
+      return { message: "1" };
+    }
+  }
+
   @Sse('app-updated')
   @OnEvent('app.statusUpdated')
   sseAppUpdated(updatedApp: AndroidApp | null): Observable<MessageEvent> {
-    //return interval(1000).pipe(map((_) => ({ data: {message: 'updateBoolean'} } )));
-    console.log("TRIGGEREEEEEED");
-    if(updatedApp !== undefined && updatedApp !== null) {
-      console.log("update SSE triggered");
-      return interval(2000).pipe(map((_) => ({ data: updatedApp } as MessageEvent)),
-      );
+
+    let observableData: MessageEvent = null;
+
+    //Construct the data to emit
+    if (!(updatedApp === undefined) && !(updatedApp === null)) {
+      observableData = { data: updatedApp };
+      console.log("App updated"/*, observableData*/);
     } else {
-      console.log("Waiting for trigger");
-      return interval(2000).pipe(map((_) => ({ data: {message: "waiting..."} } as MessageEvent)),
-      );
+      observableData = { data: "No app update" };
+      console.log("No update", observableData);
     }
+
+    return interval(5000).pipe(
+      switchMap(() => of(observableData)),
+      map((p) => ({
+        data: {
+          status: p.data,
+        }
+      }
+      )),
+    );
+    //Data stay stucked on the 'else' condition (first call i suppose)
   }
 }
